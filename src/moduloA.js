bdf3a6c01608738c4eed984210442c6ea0775c6f
Jose Luis <jlcafferata@gmail.com>
2019-02-27 09:05:25 -0300

    Project advance - JLC

--- src/moduloA.js
+++
@@ -1,5 +0,0 @@
-let a = 'soy A';
-
-module.exports = function() {
-    return a;
-}
--- src/moduloB.js
+++
@@ -1,6 +0,0 @@
-export default function() {
-    alert('Soy el default del modulo B');
-}
-export function nombre() {
-    alert('soy nombre del moduloB');
-}
